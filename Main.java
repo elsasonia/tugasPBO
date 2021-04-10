d70ed00ac59676ddfb1fc8620eecaa0fff2ccc6d
elsa180 <80958830+elsasonia@users.noreply.github.com>
2021-04-10 22:30:09 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+package PBOkuis;
+
+public class Main {
+   public static void main(String[] args) {
+        Benda produk = new Benda();
+        produk.menu();
+        produk.getnilaidata();
+    }
+} 
